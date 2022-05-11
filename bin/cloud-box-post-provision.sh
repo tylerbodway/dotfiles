0f46a781fcd6de9979441fa87ee2cfed27385e62
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2022-05-11 12:09:25 -0500

    Rework cloud-box post provisioning
    This adds a `linux.conf.yaml` profile to install desired tooling on
    my remote dev environment.
    
    I also add a post-provision bin script which will be copied and run
    after provisioning a new cloud-box.

--- bin/cloud-box-post-provision.sh
+++ bin/cloud-box-post-provision.sh
@@ -1,25 +1,54 @@
 #!/usr/bin/env bash
 
-echo "Downloading latest APT package lists"
+printf "\n==== Downloading latest apt package lists ====\n\n"
+
 sudo apt-get update
 
-echo "Installing zsh"
+printf "\n🌷 The package lists are certified fresh!\n"
+
+printf "\n==== Installing homebrew ====\n\n"
+
+# install system requirements for homebrew
+sudo apt-get install build-essential
+# install via instructions: https://brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+printf "\n🍺 You can brew now bro!\n"
+
+printf "\n==== Installing zsh ====\n\n"
+
+# install zsh as apt package
 sudo apt install -y zsh
+# change default shell from bash to zsh
 sudo chsh $(whoami) -s /usr/bin/zsh
 
-echo "Configuring bash"
-echo "export EDITOR=\"code --wait\"" >> ~/.bashrc
-echo "alias rr=\"bin/rails routes\"" >> ~/.bashrc
-echo "alias rc=\"bin/rails console\"" >> ~/.bashrc
-echo "alias rdm=\"bin/rails db:migrate\"" >> ~/.bashrc
-echo "alias bay=\"bundle && yarn\"" >> ~/.bashrc
-echo "alias devlog=\"tail -f log/development.log\"" >> ~/.bashrc
+printf "\n🐚 Ah swell, now you got that z shell!\n"
 
-echo "Configuring git"
-wget "https://raw.githubusercontent.com/tylerbodway/dotfiles/main/gitconfig" -L -O "$HOME/.gitconfig"
-wget "https://raw.githubusercontent.com/tylerbodway/dotfiles/main/gitconfig_company" -L -O "$HOME/Code/.gitconfig_company"
+printf "\n==== Installing oh-my-zsh framework ====\n\n"
 
-# echo "Cloning extra repos"
-# cd ~
-# git clone git@github.com:ministrycentered/pco-api.git
-# git clone git@github.com:ministrycentered/pco-message-bus.git
+# install via instructions: https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+printf "\n😎 My oh-my, these tools are fly!\n"
+
+printf "\n==== Installing spaceship prompt ====\n\n"
+
+# clone zsh theme into custom folder
+git clone git@github.com:spaceship-prompt/spaceship-prompt ~/.oh-my-zsh/custom/themes/spaceship-prompt --depth=1
+# symlink .zsh-theme from cloned repository
+ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
+
+printf "\n🚀 T-minus 0 seconds to a great prompt!\n"
+
+printf "\n==== Installing dotfiles ====\n\n"
+
+# clone dotfiles and enter directory
+git clone git@github.com:tylerbodway/dotfiles ~/dotfiles && cd ~/dotfiles
+# ensure newly installed brew command is available
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# run installation script in dotfiles
+./install linux
+
+printf "\n🖋  Crossed your T's and dotted your files!\n"
+
+printf "\n✅ post-provision is complete."
