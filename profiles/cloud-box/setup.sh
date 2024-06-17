9d3ffd248d2e13894d36c87f7b1f24c1c027abc0
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-06-17 16:24:36 -0500

    Fixes for setup scripts
    My cloud-box setup script wanted sudo for initially installing Homebrew
    and I thought I would switch out ~ for $HOME
    
    Also these files were not 755 executable, so I made that change as well.

--- profiles/cloud-box/setup.sh
+++ profiles/cloud-box/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x # print commands being run
+
 # Download latest apt package lists
 sudo apt-get update
 
@@ -7,7 +9,7 @@ sudo apt-get update
 sudo apt-get install build-essential
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Ensure brew command is available
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
@@ -19,8 +21,8 @@ sudo apt install -y zsh
 sudo chsh $(whoami) -s /usr/bin/zsh
 
 # Clone dotfiles repo
-git clone --recurse-submodules git@github.com:tylerbodway/dotfiles ~/.dotfiles
+git clone --recurse-submodules git@github.com:tylerbodway/dotfiles $HOME/.dotfiles
 
 # Run installation script
-cd ~/.dotfiles
+cd $HOME/.dotfiles
 ./install cloud-box
