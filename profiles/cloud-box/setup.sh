799d644b0e8d0a28f050edbe0b89e599e736f08b
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-06-18 09:28:26 -0500

    Fixes to cloud-box setup script
    First, the homebrew installation needs CI=1 to run in non-interactive
    mode so it's not looking for sudo access. Then I'm /dev/null-ing the
    output because I don't want the install.sh script printed out.
    
    Second, I'm gonna use brew to install Git and ZSH since I have it,
    rather than apt.
    
    Third, for some reason the git submodules were not being downloaded
    properly. I'm not sure if that's due to the Git version or what, but
    I'm gonna try a more straightforward way of installing theme.

--- profiles/cloud-box/setup.sh
+++ profiles/cloud-box/setup.sh
@@ -5,24 +5,22 @@ set -x # print commands being run
 # Download latest apt package lists
 sudo apt-get update
 
-# Install system requirements for Homebrew
-sudo apt-get install build-essential
-
-# Install Homebrew
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Homebrew in non-interactive mode
+CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
 
 # Ensure brew command is available
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-# Install ZSH
-sudo apt install -y zsh
+# Install Git & ZSH
+brew install git zsh
 
 # Change default shell to ZSH
-sudo chsh $(whoami) -s /usr/bin/zsh
+sudo chsh $(whoami) -s $(which zsh)
 
 # Clone dotfiles repo
-git clone --recurse-submodules git@github.com:tylerbodway/dotfiles $HOME/.dotfiles
+git clone https://github.com/tylerbodway/dotfiles.git $HOME/.dotfiles
+cd $HOME/.dotfiles
+git submodule update --init --recursive
 
 # Run installation script
-cd $HOME/.dotfiles
 ./install cloud-box
