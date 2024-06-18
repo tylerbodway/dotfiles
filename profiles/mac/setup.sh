ea2ca2e1453fbdf0fa933f689466c87e696d6dee
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-06-18 12:03:38 -0500

    Follow same setup.sh strategies for mac

--- profiles/mac/setup.sh
+++ profiles/mac/setup.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env zsh
 
+TERM=xterm-256color
+BLUE=$(tput setaf 4)
+NC=$(tput sgr0)
+
+run() {
+  local cmd=$1
+  echo -e "\n⚡ ${BLUE}${cmd}${NC}"
+  eval "${cmd}"
+}
+
 # Install Xcode Command Line Tools
-xcode-select --install
+run 'xcode-select --install'
+
+# Wait until Xcode Command Line Tools is finished installing
+until $(xcode-select --print-path &> /dev/null); do
+  sleep 5;
+done
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+run '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
 
 # Ensure brew command is available
-eval "$(/opt/homebrew/bin/brew shellenv)"
+run 'eval "$(/opt/homebrew/bin/brew shellenv)"'
 
 # Clone dotfiles repo
-git clone --recurse-submodules git@github.com:tylerbodway/dotfiles ~/.dotfiles
+run 'git clone https://github.com/tylerbodway/dotfiles.git $HOME/.dotfiles'
+run 'cd $HOME/.dotfiles'
+run 'git submodule update --init --recursive'
 
 # Run installation script
-cd ~/.dotfiles
-./install mac
+run './install mac'
