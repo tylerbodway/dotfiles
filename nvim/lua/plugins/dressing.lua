c023acda975927f2057044becbe81c8efb71ea7d
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-06-27 12:14:02 -0500

    Try NvChad distro for Neovim

--- nvim/lua/plugins/dressing.lua
+++
@@ -1,7 +0,0 @@
--- Dressing is an improvement on the default vim.ui interfaces
--- https://github.com/stevearc/dressing.nvim
-
-return {
-  'stevearc/dressing.nvim',
-  event = 'VeryLazy',
-}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,38 @@
 return {
-  'nvim-lua/plenary.nvim', -- lua functions lots of plugins use
-  'nvim-tree/nvim-web-devicons', -- icons lots of plugins use
-  'tpope/vim-sleuth', -- detect tabstop and shiftwidth automatically
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("nvchad.configs.lspconfig").defaults()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+  --
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
