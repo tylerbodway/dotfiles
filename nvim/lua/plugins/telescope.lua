81884e2f3c3c76775f206bf52ad0876b31985430
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-07-02 14:58:42 +0000

    Change telescope layout
    It's more familiar to have the prompt on top for me.

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+    },
+  },
+}
