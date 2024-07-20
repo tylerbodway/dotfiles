6c99b9b30d176b42c954decfb9277543e9ed33a9
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-07-20 10:23:01 -0500

    Fix period indent issue in ruby files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/3363#issuecomment-1538607633
+vim.cmd("autocmd FileType ruby setlocal indentkeys-=.")
