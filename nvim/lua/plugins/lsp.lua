bb7365bc067f0c9803905f8c926e38740bb541f4
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-07-20 15:53:48 +0000

    Don't install ruby language servers via mason
    I keep having trouble with Mason installing these tools. I'm just going
    to handle them manually for now.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,12 +7,14 @@ return {
         autostart = false,
       },
       ruby_lsp = {
+        mason = false, -- gem install ruby-lsp
         init_options = {
           linters = { "rubocop" },
           formatter = "rubocop",
         },
       },
       syntax_tree = {
+        mason = false, -- gem install syntax_tree
         cmd = { "bundle", "exec", "stree", "lsp" },
       },
       emmet_language_server = {},
