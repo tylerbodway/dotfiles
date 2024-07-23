ce17e399365d2d7e3a77c8689ef7e87af15322cd
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-07-23 14:54:45 -0500

    Add planning center dev database connections

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,10 @@
 -- Add any additional options here
 
 vim.opt.swapfile = false
+
+-- https://github.com/kristijanhusak/vim-dadbod-ui?tab=readme-ov-file#via-gdbs-global-variable
+vim.g.dbs = {
+  groups_development = "mysql://root@pco.test:3306/groups_development",
+  account_center_development = "mysql://root@pco.test:3306/account_center_development",
+  chat_development = "mysql://root@pco.test:3306/chat_development",
+}
