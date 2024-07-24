53d74454c2e02bc836a909cc07d83deee222dd4f
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-07-24 09:18:31 -0500

    Remove disabled headlines.nvim plugin
    This has been removed from LazyVim so there's no need to explicitly
    disable it anymore.

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
-  { "lukas-reineke/headlines.nvim", enabled = false },
   { "iamcco/markdown-preview.nvim", enabled = false },
 }
