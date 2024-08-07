2cc63386f48f19bae3285d4e9e21bc674e6c2f28
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2024-08-07 19:53:30 +0000

    Update Neovim local leader key
    This is currently conflicting with the key I use for NeoTree

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Default is \ which I use for NeoTree
+vim.g.maplocalleader = ","
+
 vim.opt.swapfile = false
 
 -- https://github.com/kristijanhusak/vim-dadbod-ui?tab=readme-ov-file#via-gdbs-global-variable
