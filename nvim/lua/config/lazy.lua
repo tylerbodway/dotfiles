a1f7b7474bc20d4c53a51025ad2e75ece49fc105
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2025-01-15 14:44:48 -0600

    Change lazy.nvim update frequency to once per day

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -37,7 +37,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, frequency = 86400 }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
