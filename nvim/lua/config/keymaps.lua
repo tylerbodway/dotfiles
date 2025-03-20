e52ceec90832a5f585743ed8c0da3bd5051e440e
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2025-03-20 12:10:10 -0500

    Remove LazyVim lazygit keymap deletions

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,16 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- lazygit
--- https://github.com/LazyVim/LazyVim/blob/c1b76ee235a2cccfdf6370ecfca57bdacd5fe6258/lua/lazyvim/config/keymaps.lua#L132-L148
-vim.keymap.del("n", "<leader>gG")
-vim.keymap.del("n", "<leader>gL")
-vim.keymap.del("n", "<leader>gb")
-vim.keymap.del("n", "<leader>gB")
-vim.keymap.del("n", "<leader>gc")
-vim.keymap.del("n", "<leader>ge")
-vim.keymap.del("n", "<leader>gs")
-
 -- floating terminal
 -- https://github.com/LazyVim/LazyVim/blob/c1b76ee235a2cccff6370ecfca57bdacd5fe6258/lua/lazyvim/config/keymaps.lua#L160-L165
 vim.keymap.del("n", "<leader>ft")
