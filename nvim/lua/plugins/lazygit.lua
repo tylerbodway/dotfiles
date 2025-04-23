377fab19837cdc9cbfc1845a33c43db347cda969
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2025-04-23 15:32:52 -0500

    Remove telescope config
    LazyVim uses snacks by default now, no need for telescope

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -13,9 +13,6 @@ return {
     { "<leader>gl", "<cmd>LazyGitFilter<cr>", desc = "Lazygit Log" },
     { "<leader>gf", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "Lazygit Log (Current File)" },
   },
-  config = function()
-    require("telescope").load_extension("lazygit")
-  end,
   init = function()
     vim.g.lazygit_floating_window_scaling_factor = 0.95
   end,
