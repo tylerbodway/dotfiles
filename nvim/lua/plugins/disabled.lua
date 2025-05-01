adfe7b7b332535dcf1ef77b3789174f78af977be
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2025-05-01 09:47:35 -0500

    Disable render-markdown plugin
    While editing markdown, I don't really want to be in a hybrid preview
    state as it's jarring to edit the formatting characters themselves. I
    will disable this feature from the render-markdown plugin. I don't mind
    having markdown-preview which will open a browser tab with my markdown
    preview.

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
-  { "iamcco/markdown-preview.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
