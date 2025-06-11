1b054378d4e09a04333188b01555ad2ea4446957
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2025-06-11 13:55:25 -0500

    Split ai config to copilot and copilot-chat
    I'll eventually want to swap out the chat plugin, but keep the copilot
    plugin.

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -23,10 +23,4 @@ return {
       },
     },
   },
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      copilot_node_command = vim.fn.expand("$XDG_DATA_HOME") .. "/asdf/installs/nodejs/23.11.0/bin/node",
-    },
-  },
 }
---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      copilot_node_command = vim.fn.expand("$XDG_DATA_HOME") .. "/asdf/installs/nodejs/23.11.0/bin/node",
+    },
+  },
+}
