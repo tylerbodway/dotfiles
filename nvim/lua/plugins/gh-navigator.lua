034c55227d996e3fed6b4324faf8ae577a542f12
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2025-06-11 11:06:40 -0500

    Only load gh-navigator with git repo
    Whenever I open Neovim in a directory that doesn't have a git repo, I
    get a warning notification stating the plugin is only for that scenario.
    I can add this condition to the lazy plugin so it won't load without a
    git repo.
    
    Note: I was getting linting errors on using vim.loop. AI explained
    vim.loop is not standard lua but exposed by Neovim's Lua API and
    available at runtime, but static analyzers can't know that. This is safe
    to disable this warning.

--- nvim/lua/plugins/gh-navigator.lua
+++ nvim/lua/plugins/gh-navigator.lua
@@ -2,4 +2,9 @@ return {
   "wassimk/gh-navigator.nvim",
   version = "*",
   config = true,
+  cond = function()
+    -- Check if the current directory has a git repository
+    ---@diagnostic disable-next-line: undefined-field
+    return vim.loop.fs_stat(vim.loop.cwd() .. "/.git") ~= nil
+  end,
 }
