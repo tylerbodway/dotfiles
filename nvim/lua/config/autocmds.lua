ce94353be3425d8c6b0aca90021405ba739dfce7
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2025-07-18 23:03:41 -0700

    Add nvim autocmd for zprofile filetype

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,3 +4,11 @@
 
 -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3363#issuecomment-1538607633
 vim.cmd("autocmd FileType ruby setlocal indentkeys-=.")
+
+-- set zprofile filetype to zsh for highlighting
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "zprofile",
+  callback = function()
+    vim.bo.filetype = "zsh"
+  end,
+})
