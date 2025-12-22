861dafc64e92ee23250beda996b12092b4eb11e8
Tyler Bodway <9326912+tylerbodway@users.noreply.github.com>
2025-12-22 10:42:44 -0600

    Always open nvim help in vertical split

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,6 +1,8 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight yanked text",
-  callback = function()
-    vim.highlight.on_yank({ timeout = 150 })
-  end,
+  callback = function() vim.highlight.on_yank({ timeout = 150 }) end,
 })
+
+-- Always open :help in vertical split
+vim.cmd.cnoreabbrev("help vert help") -- :help to :vert help
+vim.cmd.cnoreabbrev("h vert help") -- :h to :vert help
